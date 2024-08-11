78c9817123be873c2fa79bf3de3c38c5c466c9cd
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-08-11 11:45:10 +0300

    Implemented hero section (WIP)

--- src/ui/pages/WelcomePage/components/HeroSection/HeroSection.tsx
+++ src/ui/pages/WelcomePage/components/HeroSection/HeroSection.tsx
@@ -1,20 +1,31 @@
 import { FC } from 'react';
 
+import Image from 'next/image';
+
+import WOMAN_RECEIPT_IMAGE from '@public/woman-receipt.svg';
+
 export const HeroSection: FC = () => {
   return (
-    <div className="py-5 px-4 flex w-full flex-col items-center justify-center gap-5 xs:gap-6 sm:gap-7 md:gap-10 md:py-8 md:px-5 lg:gap-12">
-      <div className="flex flex-col items-center justify-center gap-3 md:gap-6 lg:gap-10">
-        <h1 className="text-[24px] text-center font-black w-[340px] font-inter xs:text-3xl xs:w-[380px] sm:text-4xl sm:w-[450px] md:text-5xl md:w-[600px] lg:text-6xl lg:w-[750px]">
-          Create Custom Receipts. Anywhere. Anytime.
-        </h1>
-        <h3 className="text-sm w-80 text-center xs:text-base xs:w-96 sm:text-lg sm:w-[400px] md:text-xl md:w-[440px] lg:text-2xl lg:w-[530px]">
-          Quickly generate professional, branded receipts. Ideal for small
-          businesses and entrepreneurs needing a fast, easy solution.
-        </h3>
+    <div className="flex flex-col-reverse items-center py-5 px-4 md:py-8 md:px-5 xl:flex-row">
+      <div className="flex w-full h-5/6 flex-col items-center justify-center gap-5 xs:gap-6 sm:gap-7 md:gap-10 lg:gap-12 xl:h-[90vh] xl:w-2/3">
+        <div className="flex flex-col items-center justify-center gap-3 md:gap-6 lg:gap-10">
+          <h1 className="text-[24px] text-center font-black w-[340px] font-inter xs:text-3xl xs:w-[380px] sm:text-4xl sm:w-[450px] md:text-5xl md:w-[600px] lg:text-6xl lg:w-[750px]">
+            Create Custom Receipts. Anywhere. Anytime.
+          </h1>
+          <h3 className="text-base w-[320px] text-center xs:w-96 sm:text-lg sm:w-[400px] md:text-xl md:w-[440px] lg:text-2xl lg:w-[530px]">
+            Quickly generate professional, branded receipts. Ideal for small
+            businesses and entrepreneurs needing a fast, easy solution.
+          </h3>
+        </div>
+        <button className="btn bg-orange-600 hover:bg-orange-700 btn-md text-white sm:btn-lg">
+          Explore templates
+        </button>
       </div>
-      <button className="btn btn-primary btn-md text-white sm:btn-lg">
-        Explore templates
-      </button>
+      <div className="flex items-center justify-center w-1/2 xl:w-1/3">
+        <div className="w-5/6">
+          <Image src={WOMAN_RECEIPT_IMAGE} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
