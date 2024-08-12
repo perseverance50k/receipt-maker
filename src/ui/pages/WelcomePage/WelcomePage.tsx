40673ee79e89c8d2688ed5db408349b5b55497b4
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-08-12 19:14:16 +0300

    Updated welcome page, assigned link to templates page to 'explore' button

--- src/ui/pages/WelcomePage/WelcomePage.tsx
+++ src/ui/pages/WelcomePage/WelcomePage.tsx
@@ -5,7 +5,7 @@ import { HeroSection } from './components';
 export const WelcomePage: NextPage = () => {
   return (
     <main>
-      <div className="flex w-full flex-col gap-5">
+      <div className="flex w-full h-screen flex-col gap-5">
         <HeroSection />
       </div>
     </main>
--- src/ui/pages/WelcomePage/components/HeroSection/HeroSection.tsx
+++ src/ui/pages/WelcomePage/components/HeroSection/HeroSection.tsx
@@ -7,9 +7,9 @@ import WOMAN_RECEIPT_IMAGE from '@public/woman-receipt.svg';
 export const HeroSection: FC = () => {
   return (
     <div className="flex flex-col-reverse items-center py-5 px-4 md:py-8 md:px-5 xl:flex-row">
-      <div className="flex w-full h-5/6 flex-col items-center justify-center gap-5 xs:gap-6 sm:gap-7 md:gap-10 lg:gap-12 xl:h-[90vh] xl:w-2/3">
+      <div className="flex w-full h-5/6 flex-col items-center justify-center gap-5 xs:gap-6 sm:gap-7 md:gap-10 lg:gap-12 xl:h-[80vh] xl:w-2/3">
         <div className="flex flex-col items-center justify-center gap-3 md:gap-6 lg:gap-10">
-          <h1 className="text-[24px] text-center font-black w-[340px] font-inter xs:text-3xl xs:w-[380px] sm:text-4xl sm:w-[450px] md:text-5xl md:w-[600px] lg:text-6xl lg:w-[750px]">
+          <h1 className="text-[24px] font-inter text-center font-black w-[340px] xs:text-3xl xs:w-[380px] sm:text-4xl sm:w-[450px] md:text-5xl md:w-[600px] lg:text-6xl lg:w-[750px]">
             Create Custom Receipts. Anywhere. Anytime.
           </h1>
           <h3 className="text-base w-[320px] text-center xs:w-96 sm:text-lg sm:w-[400px] md:text-xl md:w-[440px] lg:text-2xl lg:w-[530px]">
@@ -18,7 +18,7 @@ export const HeroSection: FC = () => {
           </h3>
         </div>
         <button className="btn bg-orange-600 hover:bg-orange-700 btn-md text-white sm:btn-lg">
-          Explore templates
+          <a href="/templates">Explore templates</a>
         </button>
       </div>
       <div className="flex items-center justify-center w-1/2 xl:w-1/3">
